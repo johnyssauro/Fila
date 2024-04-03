5936e1ff9971a1f1b8ad7752562b3c29a521a035
João <joaopedronoronhadutra@gmail.com>
2024-04-03 00:40:33 -0300

    Updating filaMain.c file

--- filaMain.c
+++ filaMain.c
@@ -1,3 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fila.h"
+
+typedef struct elemento {
+
+    int valor;
+    struct elemento * prox;
+
+} elemento;
+
+typedef struct fila {
+
+    elemento * inicio;
+    elemento * final;
+
+} fila;
+
+int main() {
+
+    fila *f = cria_fila();
+
+    push(&f, 4);
+    push(&f, 6);
+    push(&f, 1);
+    push(&f, 2);
+
+    fila_imprime(&f);
+
+    printf("%d\n", pop(&f));
+    printf("%d\n", pop(&f));
+    printf("%d\n", pop(&f));
+    printf("--------------------\n");
+
+    fila_imprime(&f);
+
+    push(&f, 6);
+    push(&f, 10);
+    push(&f, 21);
+
+    fila_imprime(&f);
+
+    printf("%d\n", pop(&f));
+    printf("%d\n", pop(&f));
+    printf("%d\n", pop(&f));
+
+    fila_libera(&f);
+
+
+    return 0;
+}
